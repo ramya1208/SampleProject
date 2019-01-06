72ce75dbf722a16efc03b526db4410756866cdd5
Dell <Dell@10.0.0.183>
2019-01-05 23:59:44 -0600

    commit

---
+++ SampleGitProject/src/Sample/GIT.java
@@ -0,0 +1,18 @@
+package Sample;
+
+public class GIT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		add();
+
+	}
+	public static void add(){
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println(c);
+				
+	}
+
+}
